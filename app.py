0733d398586da40c9e8d4fd1d78c8e692aa242b6
parthsaini08 <56781801+parthsaini08@users.noreply.github.com>
2021-03-31 08:14:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def upload_file():
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 if __name__ == '__main__':
-   app.run(debug = True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
